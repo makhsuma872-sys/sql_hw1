b4584a7a982f196d52603f15bf216e2d66e19daa
makhsuma <makhsuma872@gmail.com>
2025-09-01 13:28:51 +0500

    Create task1.sql

---
+++ lesson-1/homework/task1.sql
@@ -0,0 +1,35 @@
+Answers,easy
+1.DATA-is simply raw facts or info, it can be numbers, words,images,sounds,etc
+DATABASE-is an organised collection of data that is stored easily to find,manage and update
+RELATIONAL DATABASE-ia a special type of database that stores data in tables and connected to each other FOR EX:Students table stores student details
+TABLE-is the basic structure inside a relational database, it looks like a spreadsheet with rows and columns. column:name,age,etc, rows: single students details
+
+EASY 2
+FIVE KEY FEATURES OF SQL SERVER: 1 Stores information in tables  2.Protects data with user accounts and permissions 3.Allows you to back up and restore data 4.Handles large amounts of data quickly 5.Connects tables together
+
+EASY 3
+There are 2 main authentication modes: 1.Windows Aunthentication 2.SQL SERVER Authentication (HONESTLY DIDNT QUITE CATCH THAT QUESTION)
+
+MEDIUM 4,5
+CREATE DATABASE SchoolDB
+CREATE TABLE Students (StudentID INT, Name VARCHAR (50), Age INT)
+INSERT INTO Students (StudentID,Name,Age) VALUES (1,'Maxsuma',19),(2,'Imona',18)
+SELECT * FROM Students
+
+MEDIUM 6 
+SQL-A language for working with data in databases (the language)
+SQL SERVER-The database management system that stores and organizes data (the database system)
+SSMS-A tool/application to connect to SQL server and write SQL queries (the tool u use to type SQL into SQL SERVER
+
+  HARD 7
+  DQL-used to read/search data from a database (select,query)
+  DML-used to work with the data inside tables (insert,update,delete)
+  DDL-used to define or change the structure of the database (create,alter,drop,truncate)
+  DCL-used to control access to the data (grant,revoke)
+  TCL-used to manage transactions,group of changes (begin tran, commit tran, rollback tran)
+
+ HARD 8 
+I DID IT IN MEDIUM 4,5
+
+HARD 9
+WE DID IT ON THE LESSON WITH TEACHER
